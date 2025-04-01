d79a5e1328f3c04d40653ae1f90840dcf9d21fa2
Mark Watkinson <markwatkinson@gmail.com>
2025-04-01 19:56:09 +0100

    Set blocks_to_swap to 34 by default
    VRAM saving strategy. 34 may be too aggressive.

--- app.py
+++ app.py
@@ -483,7 +483,8 @@ def gen_sh(
   --discrete_flow_shift 3.1582 {line_break}
   --model_prediction_type raw {line_break}
   --guidance_scale {guidance_scale} {line_break}
-  --loss_type l2 {line_break}"""
+  --loss_type l2 {line_break}
+  --blocks_to_swap 34 {line_break}"""
    
 
 
